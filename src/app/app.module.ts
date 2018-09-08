d0c0c107ee8f0c26fa74dd6a4670ae0f71c37ead
sunandakadam <sunandak2007@gmail.com>
2018-09-08 17:46:55 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule, Router} from '@angular/router';
+
+const r : Routes=[
+  {path: "",component:HomeComponent},
+  {path: "Users",component:UsersComponent},
+  {path: "Login",component:LoginComponent},
+  {path: "Register",component:RegisterComponent}
+];
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    UsersComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule .forRoot(r)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
